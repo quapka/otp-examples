3ccfcfcd307f759db45afa5bc28082fdb59f4394
quapka <quapka@gmail.com>
2018-02-14 22:39:45 +0100

    Refactor code to split logic into functions

--- python-examples/spacex_tweets.py
+++ python-examples/spacex_tweets.py
@@ -5,19 +5,36 @@ import sys
 from bs4 import BeautifulSoup
 import requests
 
+def get_page():
+    page = requests.get('https://twitter.com/spacex')
+    return page
 
-page = requests.get('https://twitter.com/spacex')
-if page.status_code != 200:
-    print('Could not fetch the tweets. Exiting..')
-    sys.exit(1)
+def parse_page(page):
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
 
-soup = BeautifulSoup(page.content, 'html.parser')
-tweets = soup.find_all(attrs={'class': 'tweet', 'data-name': 'SpaceX'})
+def get_tweets(parsed_page):
+    tweets = parsed_page.find_all(attrs={'class': 'tweet', 'data-name': 'SpaceX'})
+    return tweets
 
-last_tweets = []
-for tweet in tweets[:3]:
-    last_tweets.append(tweet.find(attrs={'class': 'TweetTextSize'}).text)
+def main():
+    page = get_page()
+    if page.status_code != 200:
+        print('Could not fetch the tweets. Exiting..')
+        sys.exit(1)
 
-print('Last three SpaceX tweets')
-for tweet_text in last_tweets:
-    print(tweet_text)
+    parsed_page = parse_page(page)
+    tweets = get_tweets(parsed_page)
+
+
+    last_tweets = []
+    for tweet in tweets[:3]:
+        last_tweets.append(tweet.find(attrs={'class': 'TweetTextSize'}).text)
+
+    print('Last three SpaceX tweets:')
+    for tweet_text in last_tweets:
+        print(tweet_text)
+
+
+if __name__ == '__main__':
+    main()
