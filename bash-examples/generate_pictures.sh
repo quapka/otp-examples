a25c87f0b0a89b7bc68df2f01b63c87404ea3d27
quapka <quapka@gmail.com>
2018-02-14 21:50:40 +0100

    Log successfull generating of a picture

--- bash-examples/generate_pictures.sh
+++ bash-examples/generate_pictures.sh
@@ -10,4 +10,7 @@ fi
 echo "Generating dummy pictures.."
 for i in {01..10}; do
     touch "$DIR/IMG_$i.JPG";
+    if [ "$?" -eq 0 ]; then
+        echo "... succesfully generated $DIR/IMG-$I.JPG";
+    fi
 done
